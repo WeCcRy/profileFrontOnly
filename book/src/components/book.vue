bfb2d176f6e0845bee57d4a7f4a7b0997ae2a045
WeCcRy <18221653657@163.com>
2022-10-29 19:29:29 +0800

    update

--- book/src/components/book.vue
+++ book/src/components/book.vue
@@ -20,7 +20,11 @@
       </a-col>
       <a-col :span="18"></a-col>
       <a-col :span="2"><a-button type="link" @click="showDrawer">我的图书报告</a-button></a-col>
-      <a-col :span="1"></a-col>
+      <a-col :span="1">
+        <a-button v-if="year==2022" type="primary" size="small" @click="showModal">
+          <template #icon><plus-outlined /></template>
+        </a-button>
+      </a-col>
     </a-row>
     </a-layout-head>
     <a-layout-content style="margin: 0 16px">
@@ -65,7 +69,7 @@
         </template>
       </a-table>
     </a-layout-content>
-  <a-drawer width="640" placement="right" :closable="false" :visible="visible" @close="onClose">
+  <a-drawer width="640" placement="right" :closable="false" :visible="drawerVisible" @close="onClose">
     <p :style="[pStyle, pStyle2]">我的{{year}}</p>
     <p :style="pStyle">Personal</p>
     <a-row>
@@ -75,13 +79,53 @@
       </a-col>
     </a-row>
   </a-drawer>
+    <a-modal v-model:visible="formVisible" title="添加图书" @ok="handleOk">
+      <a-form :model="addBookInfo" @finish="onFinish">
+        <a-form-item name="ISBN" label="ISBN" has-feedback required :rules="[{type: 'string', min: 13, max: 13 ,message:'请输入13位ISBN码'}]">
+          <a-input v-model:value="addBookInfo.ISBN" />
+        </a-form-item>
+        <a-form-item name="isFiction" label="小说(是/否)">
+          <a-switch v-model:checked="addBookInfo.isFiction" />
+        </a-form-item>
+        <a-form-item name="type" label="标签">
+          <a-checkbox-group v-model:value="addBookInfo.type">
+            <a-checkbox value="推理" name="type">推理</a-checkbox>
+            <a-checkbox value="历史" name="type">历史</a-checkbox>
+            <a-checkbox value="纪实" name="type">纪实</a-checkbox>
+            <a-checkbox value="社会" name="type">社会</a-checkbox>
+            <a-checkbox value="科幻" name="type">科幻</a-checkbox>
+            <a-checkbox value="哲学" name="type">哲学</a-checkbox>
+          </a-checkbox-group>
+        </a-form-item>
+        <a-form-item name="bookRate" label="评分">
+          <a-input-number v-model:value="addBookInfo.bookRate" :min="0" :max="5" :precision="2" :step="0.1"/>
+          <a-rate :value="bookRateVShow" allow-half disabled style="margin-left: 20px"/>
+        </a-form-item>
+        <a-form-item :wrapper-col="{ span: 14, offset: 7 }">
+          <a-button @click="bookClear">清空</a-button>
+          <a-button type="primary" html-type="submit" style="margin-left: 10px">查看详细信息</a-button>
+        </a-form-item>
+      </a-form>
+      <a-card style="width: 190px;margin:auto" v-if="isChecked" >
+        <template #cover>
+          <img :src="bookAddDetail.imgUrl" style="width: 190px;height: 263px" alt="加载中..."/>
+        </template>
+        <a-card-meta :title="bookAddDetail.Title">
+          <template #description>{{ bookAddDetail.Author }}</template>
+        </a-card-meta>
+      </a-card>
+      <template #footer>
+        <a-button key="back" @click="handleCancel">取消</a-button>
+        <a-button key="submit" type="primary" @click="handleOk" :disabled="!isChecked">{{bookAddConfirm}}</a-button>
+      </template>
+    </a-modal>
   </a-layout>
 </template>
 <script>
 
-import { DownOutlined,SmileTwoTone } from '@ant-design/icons-vue';
+import { DownOutlined,SmileTwoTone,PlusOutlined } from '@ant-design/icons-vue';
 import { message } from 'ant-design-vue';
-import {defineComponent, ref, watch} from 'vue';
+import {defineComponent, ref, watch, reactive, toRaw} from 'vue';
 import axios from 'axios'
 import booklist2021 from '../assets/book2021'
 import booklist2022 from '../assets/book2022'
@@ -142,12 +186,13 @@ const columns = [
 ];
 
 const data2021 = booklist2021
-const data2022 = booklist2022
+let data2022 = booklist2022
 
 export default defineComponent({
   components: {
     DownOutlined,
     SmileTwoTone,
+    PlusOutlined,
   },
   setup() {
     let year=ref("2022")
@@ -157,7 +202,12 @@ export default defineComponent({
     //https://jike.xyz/jiekou/isbn.html#%E8%BF%94%E5%9B%9E%E5%8F%82%E6%95%B0%E8%AF%B4%E6%98%8E
     //私人接口APIKEY，如无法获得数据，需手动更改
     const apikey="13294.206eaa07ed2ea8b374cf076241a2024c.f57bd57f2303f9557c040b0365744e1c"
-    const visible = ref(false);
+    const drawerVisible = ref(false)
+    const formVisible = ref(false)
+    let ISBNArray=[]
+    let isChecked=ref(false)
+    let bookRateVShow=ref(3)
+    let bookAddConfirm=ref("请先查看书本详细信息")
     const pStyle = {
       fontSize: '16px',
       color: 'rgba(0,0,0,0.85)',
@@ -168,30 +218,100 @@ export default defineComponent({
     const pStyle2 = {
       marginBottom: '24px',
     };
-
+    let addBookInfo = reactive({
+      ISBN: '',
+      isFiction: true,
+      type: [],
+      bookRate: 3,
+    });
+    let bookAddDetail=reactive({
+      ISBN:'',
+      imgUrl:'',
+      Title:'',
+      Author:'',
+    })
+    const onSubmit = () => {
+      console.log('submit!', addBookInfo);
+    };
+    //通用清空方法
+    const objClear=(obj)=>{
+      Object.keys(obj).map(key=>{
+        delete obj[key]
+      })
+    }
     const showDrawer = () => {
-      visible.value = true;
+      drawerVisible.value = true;
     };
     const onClose = () => {
-      visible.value = false;
+      drawerVisible.value = false;
     };
     const success = () => {
       message.success('This is a success message');
     };
-    function handleMenuClick(e){
+    const handleMenuClick=(e)=>{
       year.value=e.key
     }
+    const showModal = () => {
+      formVisible.value = true;
+      data2022.forEach(ele=>{
+        ISBNArray.push(ele.ISBN)
+      })
+    };
+    const bookClear=()=>{
+      addBookInfo.ISBN=''
+      addBookInfo.bookRate=0
+      addBookInfo.isFiction=true
+      addBookInfo.type=[]
+      isChecked.value=false
+      objClear(bookAddDetail)
+    }
+    const handleCancel = () => {
+      formVisible.value = false;
+    };
+    const handleOk = () => {
+      console.log(toRaw(addBookInfo));
+      let flag=0
+      ISBNArray.forEach(ele=>{
+        if(ele==addBookInfo.ISBN&&flag==0){
+          flag=1
+        }
+      })
+      if(flag==1){
+        message.error("该ISBN已存在")
+      }else{
+        message.success("添加成功")
+        formVisible.value=false
+      }
+    };
+    const onFinish = values => {
+      isChecked.value=true
+      getDetailsAdd(values.ISBN)
+      console.log('Success:', values);
+    };
     function getDetails(ISBN){
       isLoading.value=true
       bookDetail.value=""
       photoUrl.value=""
       axios.get(`https://api.jike.xyz/situ/book/isbn/${ISBN}?apikey=${apikey}`)
           .then(res=>{
+            let data=res.data.data
             isLoading.value=false
-            if(res.data.data.description.length>100) {
-              bookDetail.value = res.data.data.description.slice(0,100)+"..."
+            if(data.description.length>100) {
+              bookDetail.value = data.description.slice(0,100)+"..."
             }
-            photoUrl.value=res.data.data.photoUrl
+            photoUrl.value=data.photoUrl
+          }).catch(err=>{
+        console.log(err);
+      })
+    }
+    function getDetailsAdd(ISBN){
+      axios.get(`https://api.jike.xyz/situ/book/isbn/${ISBN}?apikey=${apikey}`)
+          .then(res=>{
+            console.log(res.data.data)
+            let data=res.data.data
+            bookAddDetail.imgUrl='https://images.weserv.nl/?url='+data.photoUrl
+            bookAddDetail.Author= data.author
+            bookAddDetail.Title=data.name
           }).catch(err=>{
         console.log(err);
       })
@@ -224,15 +344,43 @@ export default defineComponent({
     watch(year,(n,o)=>{
       message.success("已为你切换至"+n+"年")
     })
+    watch(isChecked,()=>{
+      if(isChecked.value==false){
+        bookAddConfirm.value="请先查看书本详细信息"
+      }else{
+        bookAddConfirm.value="提交"
+      }
+    })
+    watch(() => addBookInfo.bookRate,()=>{
+      let intRate=parseInt(addBookInfo.bookRate)
+      if(addBookInfo.bookRate%1>=0.5){
+        bookRateVShow.value=intRate+0.5
+      }else{
+        bookRateVShow.value=intRate
+      }
+    })
     return {
       handleMenuClick,
       getDetails,
-      visible,
+      drawerVisible,
+      formVisible,
       pStyle,
       pStyle2,
+      addBookInfo,
+      isChecked,
+      bookRateVShow,
+      bookAddConfirm,
+      bookAddDetail,
+      bookClear,
       showDrawer,
       onClose,
       success,
+      showModal,
+      handleCancel,
+      handleOk,
+      getDetailsAdd,
+      onSubmit,
+      onFinish,
       data2021,
       data2022,
       isLoading,
